fd34ad427ade132c96e530c70356a07a3cf8642a
Neill Henning <neillhenning@gmail.com>
2015-01-29 15:43:42 +0200

    fixed buggy.py

--- buggy.py
+++ buggy.py
@@ -3,7 +3,7 @@ number1 = random.randint(1, 10)
 number2 = random.randint(1, 10)
 print('What is ' + str(number1) + ' + ' + str(number2) + '?')
 answer = input()
-if answer == number1 + number2:
+if int(answer) == number1 + number2:
     print('Correct')
 else:
     print('Nope! The anser is ' + str(number1 + number2))
